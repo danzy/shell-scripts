330dcc8d782849039d365d5f1ab2683dae97f177
Daniel Carraro <dcarraro@ultraserve.com.au>
2017-02-24 11:40:12 +1100

    fixed s3 url

--- clone-organisation-repositories.sh
+++ clone-organisation-repositories.sh
@@ -51,7 +51,7 @@ tar cvf ${ORGANISATION}-repositories-${DATE}.tar.bz2 .
 ## UPLOADING TAR FILE TO S3
 ####
 
-aws s3 cp ${ORGANISATION}-repositories-${DATE}.tar.bz2 s3://${BUCKETNAME}/${BUCKETPREFIX}
+aws s3 cp ${ORGANISATION}-repositories-${DATE}.tar.bz2 s3://${BUCKETNAME}/${BUCKETPREFIX}/
 
 ####
 ## CLEANING UP
