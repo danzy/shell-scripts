6bf22201489fb31d11938398d782d1bb64bb2c62
Daniel Carraro <dcarraro@ultraserve.com.au>
2017-02-27 14:41:35 +1100

    added support for https cloning (via personal access token); added support for multiple organisations (via comma separated parameter)

--- clone-organisation-repositories.sh
+++ clone-organisation-repositories.sh
@@ -1,4 +1,4 @@
-#/bin/bash
+#!/bin/bash
 
 ####
 ## CLONE-ORGANISATION-REPOSITORIES.SH
@@ -6,7 +6,7 @@
 ## This script will do the following:
 ## 1. Create a temporary directory in /tmp/ to store repositories
 ## 2. Pull a list of all GitHub repositories for a given organisation
-## 3. `git clone` that list onto disk
+## 3. `git clone` that list onto disk (using the GitHub personal access token
 ## 4. tar repositories up into one file (named with the date)
 ## 5. Upload tar file to S3
 ## 6. Delete temporary directory created in #1
@@ -29,29 +29,37 @@ DATE=`date +%Y%m%d`
 ## MAKING TEMP DIRECTORY
 ####
 
-mkdir ${TEMPDIRECTORY}
-cd ${TEMPDIRECTORY}
+rm -rf "${TEMPDIRECTORY}"
+mkdir "${TEMPDIRECTORY}"
+cd "${TEMPDIRECTORY}"
 
 ####
 ## FINDING AND CLONING ALL REPOS
 ####
 
-for i in `curl -u ${TOKEN}:x-oauth-basic -s "https://api.github.com/orgs/${ORGANISATION}/repos" | grep ssh_url | cut -d ':' -f 2-3|tr -d '",'`; 
+for x in $(echo "${ORGANISATION}" | sed "s/,/ /g")
 do
-git clone $i; 
+mkdir "${TEMPDIRECTORY}"/"${x}"
+cd "${TEMPDIRECTORY}"/"${x}"
+
+for i in `curl -u "${TOKEN}":x-oauth-basic -s "https://api.github.com/orgs/"${x}"/repos" | grep clone_url | cut -d ':' -f 2-3|tr -d '",' | cut -c10- `;
+do
+git clone https://"${TOKEN}":x-oauth-basic@"${i}";
 done
 
 ####
 ## ZIPPING UP REPOS
 ####
 
-tar cvf ${ORGANISATION}-repositories-${DATE}.tar.bz2 .
+tar cf "${x}"-repositories-"${DATE}".tar.bz2 *
 
 ####
 ## UPLOADING TAR FILE TO S3
 ####
 
-aws s3 cp ${ORGANISATION}-repositories-${DATE}.tar.bz2 s3://${BUCKETNAME}/${BUCKETPREFIX}/
+aws s3 cp "${x}"-repositories-"${DATE}".tar.bz2 s3://"${BUCKETNAME}"/"${BUCKETPREFIX}"/
+
+done
 
 ####
 ## CLEANING UP
